31ae9e3b9f29a11577e3672ae3ad01312b962813
Jimmy Vo <j.vo718@gmail.com>
2022-12-05 16:24:52 -0500

    prep for initial push to render

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+
+
+
+
+
+  
+# bellow all routes
+  get '*path', to: "static_pages#frontend_index"
+
 end
