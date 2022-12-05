df4def49de4c779bc0a435d65c95ffe62e16c135
Jimmy Vo <j.vo718@gmail.com>
2022-12-04 20:45:13 -0500

    gems & config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+# reformat case
+Jbuilder.key_format camelize: :lower
+Jbuilder.deep_format_keys true
+
 # Initialize the Rails application.
 Rails.application.initialize!
