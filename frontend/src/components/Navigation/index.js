df04a0f6e0ff4618b5144e47736dece3eaef6348
Jimmy Vo <j.vo718@gmail.com>
2022-12-06 17:35:39 -0500

    navbar arrangement

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 // import ProfileButton from './ProfileButton';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import './Navigation.css';
+import { Input } from '../Forms';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
@@ -13,23 +15,56 @@ function Navigation() {
   if (sessionUser) {
     sessionLinks = (
       // <ProfileButton user={sessionUser} />
-      <LogoutButton />
+      <>
+        <Logo/>
+        <Link>About</Link>
+        <SearchBar/>
+        <Link>Github</Link>
+        <Link>LinkedIn</Link>
+        <LogoutButton />
+    
+      </>
     );
   } else {
     sessionLinks = (
       <>
+        <DropDown/>
+        <Logo/>
+        <Link>About</Link>
+        <Link>Github</Link>
+        <Link>LinkedIn</Link>
+        <SearchBar/>
         <LoginButton />
         <NavLink to="/signup" className="button">Sign Up</NavLink>
       </>
     );
   }
 
+  function DropDown() {
+    return (
+      <h1>X</h1>
+    )
+  }
 
-  return (
-    <header className="site-header">
+  function SearchBar() {
+    return (
+      <Input class="search" type="text" placeholder="Search soon..."></Input>
+
+    )
+  }
+
+  function Logo() {
+    return (
       <NavLink exact to="/" className="nav-title">
         <h1>Question Queue</h1>
       </NavLink>
+    )
+  }
+
+
+
+  return (
+    <header className="site-header">
       <div className="session-links">
         {sessionLinks}
       </div>
