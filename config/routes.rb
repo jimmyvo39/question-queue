db385a2c10def351bb6f52443a8736d837ca9ea3
Jimmy Vo <j.vo718@gmail.com>
2022-12-09 14:08:05 -0500

    question routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # post 'api/test', to: 'application#test'
 
   namespace :api, defaults: { format: :json } do
+    resources :questions, only: [:index, :show, :create, :destroy, :update]
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
   end
 
-# bellow all routes
+# below all routes
   get '*path', to: "static_pages#frontend_index"
 
 end
