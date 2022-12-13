ce2a98c1f74a9b9509f369bb8215d669010a77ae
Jimmy Vo <j.vo718@gmail.com>
2022-12-13 09:41:17 -0500

    sidenav and temp splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,55 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
 import LoginFormPage  from "./components/LoginFormPage"
 import QuestionIndexPage  from "./components/QuestionIndexPage/index.js"
+import SideNav from "./components/SideNav";
+import SplashPage from "./components/SplashPage";
 
 function App() {
+  const sessionUser = useSelector(state => state.session.user);
+
+  let SessionLinks;
+  if (sessionUser) {
+    SessionLinks = (
+      <QuestionIndexPage />
+    );
+  } else {
+    SessionLinks = (
+
+      <SplashPage />
+    );
+  }
+
   return (
     <>
       <Navigation />
-      <Switch>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
+      <div className="body">
+        <SideNav />
 
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
+        <Switch>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
 
-        <Route path="/questions"  >
-          <>
-           <QuestionIndexPage />
-          </>
-        </Route>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
 
-        <Route exact path="/">
-          <h1>splash</h1>
-        </Route>
+          <Route path="/questions"  >
+            <>
+            <QuestionIndexPage />
+            </>
+          </Route>
 
-      </Switch>
+          <Route exact path="/">
+            {SessionLinks}
+          </Route>
+
+        </Switch>
+      </div>
     </>
   );
 }
--- frontend/src/components/QuestionIndexPage/QuestionIndexItem.js
+++ frontend/src/components/QuestionIndexPage/QuestionIndexItem.js
@@ -6,25 +6,32 @@ import { Link } from 'react-router-dom';
 const QuestionIndexItem = ({question}) => {
 
 
-
-
+  const UserName = () => {
     return(
-        <div className='question-preview'>
-          <div className='stats'>
-            <h3>votes</h3>
-            <h3>answers</h3>
-            <h3>views</h3>
-          </div>
-          <li>
-              <Link to={`questions/${question.id}`} className="question-link" >{question.title}</Link>
-              <h3 className="question-body-preview">{question.body}</h3>
-              <h3>{question.username} asked {question.createdAt}</h3>
-
-
-          </li>
-        </div>
-        
+      <h3 className='question-asker'>
+        <div className='asker'>
+          {question.username}
+        </div> 
+        asked {question.createdAt}
+      </h3>
     )
+  }
+
+  return(
+      <div className='question-preview'>
+        <div className='stats'>
+          <h3>votes</h3>
+          <h3>answers</h3>
+          <h3>views</h3>
+        </div>
+        <li className='question-preview-text'>
+            <Link to={`questions/${question.id}`} className="question-link" >{question.title}</Link>
+            <h3 className="question-body-preview">{question.body}</h3>
+            <UserName/>
+        </li>
+      </div>
+      
+  )
 }
 
 export default QuestionIndexItem
